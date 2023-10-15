be95df3663fc0214aa70231eba1e6d45897624f0
ellemenopi <126441989+elle019@users.noreply.github.com>
2023-10-15 21:56:10 +0800

    Update MainPage.xaml.cs
    Fixed DisplayAlert error when inputting string or invalid digits

--- MAUIApp3/MainPage.xaml.cs
+++ MAUIApp3/MainPage.xaml.cs
@@ -1,29 +1,46 @@
-﻿
-
-namespace MAUIApp3;
+using System.Text.RegularExpressions;
+namespace MauiApp3;
 
 public partial class MainPage : ContentPage
 {
-	
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+   
+    string phonepattern = @"\d{11}$";  /* Must input at least 11 numbers to work
+                                        * Can input 11 digits and more
+                                        * Pag naglagay ako ng code na gusto ko exact 11 numbers lang, ang nangyayari lumalabas itong syntax sa mismong displayAlert                                       
+                                        */
+    public MainPage()
+    {
+        InitializeComponent();
 
-	async void OnDial(object sender, EventArgs e)
-	{
-        try
-        {
-            PhoneDialer.Open(PhoneNum.Text);
-        }
-        catch (Exception)
-        {
-            await DisplayAlert("Invalid input", "Please enter a valid phone number", "OK");
-            throw;
-        }
     }
 
+    async void OnDial(object sender, EventArgs e)
+    { 
+      
+        try
+        {
+              
+            if (Regex.IsMatch(PhoneNum.Text, phonepattern)) //inputted the PhoneNum and set pattern
+            {
+                PhoneDialer.Open(PhoneNum.Text); //entry name
+            }
 
+            else
+            {
+                await DisplayAlert("Invalid input", "Please enter a valid phone number", "OK");
 
-}
+            };
+                      
+        }
+
+        catch (Exception ex)
+        {
+           
+                await DisplayAlert(ex.Message, "Invalid input", "Please enter a valid phone number", "OK");  
+
+            };
+
+        }
+
+    }
 
